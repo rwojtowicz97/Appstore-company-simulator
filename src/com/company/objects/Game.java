5cc9fd9e017845a1210f970bf66f95abcc87f434
rwojtowicz97 <robivw508@gmail.com>
2020-07-08 16:08:41 +0200

    removed useless variables

--- src/com/company/objects/Game.java
+++ src/com/company/objects/Game.java
@@ -25,13 +25,6 @@ public class Game implements Randomizer {
     public Scanner scan = new Scanner(System.in);
     public DateTime dateTime;
 
-    public static final int MONDAY = 1;
-    public static final int TUESDAY = 2;
-    public static final int WEDNESDAY = 3;
-    public static final int THURSDAY = 4;
-    public static final int FRIDAY = 5;
-    public static final int SATURDAY = 6;
-    public static final int SUNDAY = 7;
 
     Faker faker = new Faker();
 
